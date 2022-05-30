1bbde618bb4a6e1d9f257da58a416c96c8984e0f
Prasoon Pandey <prasoonpandey06@gmail.com>
2022-05-30 19:44:37 +0530

    Setup the navigation drawer

--- app/src/main/java/com/prasoon/mockfeed/MainActivity.java
+++ app/src/main/java/com/prasoon/mockfeed/MainActivity.java
@@ -1,20 +1,32 @@
 package com.prasoon.mockfeed;
 
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.res.TypedArray;
-import android.os.Bundle;
-
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_start, R.string.nav_stop);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         //Getting the data from the resources
         String[] userNames, userInfos, postTitles, postSmallInfos;
@@ -30,5 +42,14 @@ public class MainActivity extends AppCompatActivity {
         Adapter adapter = new Adapter(profilePics, userNames, userInfos, postPics, postTitles, postSmallInfos, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
