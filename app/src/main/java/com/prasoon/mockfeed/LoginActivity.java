e3c3da5f53376e97de39d558a668a9222a74f43a
Prasoon Pandey <prasoonpandey06@gmail.com>
2023-03-10 20:03:56 +0530

    Create backend for the login activity

--- app/src/main/java/com/prasoon/mockfeed/LoginActivity.java
+++ app/src/main/java/com/prasoon/mockfeed/LoginActivity.java
@@ -4,18 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button gotoSignup;
+    private EditText username, password;
+    private Button login, gotoSignup;
 
+    //Instance variable to hold a reference to the app's database
+    private UserDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Get the reference to the app's database
+        database = UserDatabase.getInstance(this);
+
+        username = findViewById(R.id.nameLogin);
+        password = findViewById(R.id.passwordLogin);
+
+        login = findViewById(R.id.loginBtn);
         gotoSignup = findViewById(R.id.signupBtn);
+
+        //Verify login details with the database and allow login
+        login.setOnClickListener(view -> {
+            //Get the entered username/password
+            String inputName = username.getText().toString(), inputPwd = password.getText().toString();
+
+            //Check to verify all fields have been filled
+            if(TextUtils.isEmpty(inputName) || TextUtils.isEmpty(inputPwd)){
+                Toast.makeText(this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+            }else {
+                //Verify with the database
+                new Thread(() -> {
+                    List<User> allUsers = database.userDao().getAllUsers();//Get list of all users
+
+                    boolean validUser = false;
+                    for (User user : allUsers) {
+                        if (user.getUsername().equals(inputName) && user.getPassword().equals(inputPwd)) {
+                            validUser = true;
+                            break;
+                        }
+                    }
+
+                    //If a match is found then allow the login
+                    if (validUser) {
+                        startActivity(new Intent(this, MainActivity.class));
+                    } else {
+                        runOnUiThread(() -> Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT).show());
+                    }
+
+                }).start();
+            }
+        });
 
         //If clicked, the user should be redirected to the account creation page
         gotoSignup.setOnClickListener(view -> startActivity(new Intent(this, SignupActivity.class)));
